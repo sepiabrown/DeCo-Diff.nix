05bcdc0e0129133b94977e44be8035accf381463
Su Won Park <suwonpark@haedosa.xyz>
2025-06-04 10:55:37 +0900

    rm `__pycache__` and mv apps to packages

--- packages/default.nix
+++ packages/default.nix
@@ -33,4 +33,30 @@ rec {
       torchrun ${deco-diff-env}/bin/train_deco_diff "$@"
     '';
   };
+
+  multi-gpu-train-example = pkgs.writeShellApplication {
+    name = "multi-gpu-train-example";
+    runtimeInputs = [ deco-diff-env ];
+    text = ''
+      export NCCL_P2P_DISABLE=1;
+      time torchrun \
+        --standalone \
+        --nproc_per_node=2 \
+        "$(which train_deco_diff)" \
+          --dataset pcb \
+          --data-dir ~/dataset/PCB/Huang/PCB_DATASET/PCB_gray_128 \
+          --model-size UNet_L \
+          --object-category all \
+          --augment False \
+          --ckpt-every 1 \
+          --resume-dir DeCo-Diff_pcb_all_UNet_L_128_CenterCrop/001-UNet_L \
+          --image-size 128 \
+          --center-size 128 \
+          --global-batch-size 532 \
+          --epochs 10000
+          # --nproc_per_node=1 \
+          # --resume-dir test \
+          # --global-batch-size 253 \
+    '';
+  };
 }
